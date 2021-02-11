183e1f5c2bc5e94daa31ebac13e52c445c88889f
Matias <mcharrut@fi.uba.ar>
2021-02-11 17:25:08 -0300

    Added test to GameTest

--- src/main/test/algoblocks/engine/GameTest.java
+++ src/main/test/algoblocks/engine/GameTest.java
@@ -6,13 +6,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
     @Test
-    public void workspaceAddBlockAndExecuteInPlayer(){
+    public void workspaceAddActionBlockAndExecuteInPlayer(){
         Game game = new Game();
         Block moveDown = new MoveDownBlock();
 
         game.addBlockToWorkspace(moveDown);
         
         Coordinates expectedCoordinates = new Coordinates(0, -1);
+
+        game.run();
+
+        Player playerInGame = game.getPlayer();
+        
+        assertEquals(expectedCoordinates, playerInGame.getPosition());
+    }
+
+    @Test
+    public void workspaceAddRepeatBlockAndExecuteInPlayer(){
+        Game game = new Game();
+        EffectBlock repeatTwice = new RepeatBlock(4);
+        Block moveUp = new MoveUpBlock();
+
+        repeatTwice.addBlock(moveUp);
+        game.addBlockToWorkspace(repeatTwice);
+
+        Coordinates expectedCoordinates = new Coordinates(0, 4);
+
+        game.run();
+
+        Player playerInGame = game.getPlayer();
+        
+        assertEquals(expectedCoordinates, playerInGame.getPosition());
+    }
+
+    @Test
+    public void workspaceAddInvertBlockAndExecuteInPlayer(){
+        Game game = new Game();
+        EffectBlock invert = new InvertBlock();
+        Block moveUp = new MoveUpBlock();
+        Block moveLeft = new MoveLeftBlock();
+
+        invert.addBlock(moveUp);
+        invert.addBlock(moveLeft);
+        game.addBlockToWorkspace(invert);
+
+        Coordinates expectedCoordinates = new Coordinates(1, -1);
+
+        game.run();
+
+        Player playerInGame = game.getPlayer();
+        
+        assertEquals(expectedCoordinates, playerInGame.getPosition());
+    }
+
+    @Test
+    public void workspaceAddCustomBlockAndExecuteInPlayer(){
+        Game game = new Game();
+        EffectBlock custom = new CustomBlock();
+        Block moveUp = new MoveUpBlock();
+        Block moveDown = new MoveDownBlock();
+        Block moveLeft = new MoveLeftBlock();
+
+        custom.addBlock(moveUp);
+        custom.addBlock(moveDown);
+        custom.addBlock(moveLeft);
+        game.addBlockToWorkspace(custom);
+
+        Coordinates expectedCoordinates = new Coordinates(-1, 0);
 
         game.run();
 
