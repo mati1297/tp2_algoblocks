51f2eece4409ee5678b29a85e5eeb30d1964b95f
Matias <mcharrut@fi.uba.ar>
2021-02-13 14:32:22 -0300

    Added Main, javafx config to pom.xml and updated Readme with run instruccions

---
+++ src/main/java/algoblocks/engine/Main.java
@@ -0,0 +1,17 @@
+package algoblocks.gui;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage){
+        stage.setTitle("AlgoBlocks");
+        stage.show();
+    }
+}
