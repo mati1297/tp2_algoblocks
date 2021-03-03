6c88251c1de88a8ffcc70ee96b2fc3b94acf7229
Matias <mcharrut@fi.uba.ar>
2021-03-03 16:16:04 -0300

    Refactored SetCustomBlockNameWindow

--- src/main/java/algoblocks/gui/SetCustomBlockNameWindow.java
+++ src/main/java/algoblocks/gui/SetCustomBlockNameWindow.java
@@ -13,16 +13,22 @@ import javafx.scene.layout.VBox;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.paint.Color;
 
 
 public class SetCustomBlockNameWindow extends Window{
     private static final String title = "Set custom block name";
     private static final String labelText = "New custom block text";
+    private static final String errorText = "No name inserted!";
     private static final String okText = "Ok";
     private static final String cancelText = "Cancel";
 
+    private TextField textField;
+
     private static final int WIDTH = 200;
-    private static final int HEIGHT = 100;
+    private static final int HEIGHT = 140;
 
 
 
@@ -30,28 +36,39 @@ public class SetCustomBlockNameWindow extends Window{
         super(title, WIDTH, HEIGHT);
 
         Label titleLabel = new Label(labelText);
-        
+        Label errorLabel = new Label(errorText);
+        errorLabel.setTextFill(Color.web("#ff0000", 1));
+        errorLabel.setVisible(false);
 
         
         Button okButton = new Button(okText);
+        okButton.setDefaultButton(true);
         Button cancelButton = new Button(cancelText);
+        cancelButton.setCancelButton(false);
 
         
         
-        TextField textField = new TextField();
+        textField = new TextField();
 
-        HBox buttonLayout = new HBox(okButton, cancelButton);
-        VBox mainLayout = new VBox(titleLabel, textField, buttonLayout);
+        HBox buttonLayout = new HBox(cancelButton, okButton);
+        buttonLayout.setAlignment(Pos.CENTER_RIGHT);
+        VBox mainLayout = new VBox(titleLabel, errorLabel, textField, buttonLayout);
+        mainLayout.setAlignment(Pos.CENTER_LEFT);
+        mainLayout.setPadding(new Insets(5, 5, 5, 5));
         
-        
-        Scene scene = new Scene(mainLayout); //set 
+        Scene scene = new Scene(mainLayout);
 
 
         okButton.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent actionEvent){
-                button.continueAction(textField.getText());
-                stage.close();
+                String blockName = textField.getText();
+                if(blockName == "")
+                    errorLabel.setVisible(true);
+                else{
+                    button.continueAction(blockName);
+                    stage.close();
+                }
             }
         });
         cancelButton.setOnAction(new EventHandler<ActionEvent>(){
