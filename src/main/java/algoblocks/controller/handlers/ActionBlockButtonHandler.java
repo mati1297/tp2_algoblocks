998db734b7205cb8f1ae961d2164fdf9ab41a896
Julian Garcia <juli.garcia.77@gmail.com>
2021-03-04 20:56:38 -0300

    fix refactored block button handlers

---
+++ src/main/java/algoblocks/controller/handlers/ActionBlockButtonHandler.java
@@ -0,0 +1,21 @@
+package algoblocks.controller.handlers;
+
+import algoblocks.controller.GameController;
+import algoblocks.view.workspace.WorkspaceView;
+import algoblocks.engine.block.*;
+
+import javafx.event.ActionEvent;
+
+public class ActionBlockButtonHandler extends BlockButtonHandler {
+  Block block;
+
+  public ActionBlockButtonHandler(GameController gameController, WorkspaceView workspaceView, Block block) {
+    super(gameController, workspaceView);
+    this.block = block;
+  }
+
+  public void handle(ActionEvent event) {
+    workspaceView.addActionBlock(block);
+    gameController.addActionBlockToWorkspace(block);
+  }
+}
--- src/main/java/algoblocks/view/buttons/CustomBlockButton.java
+++ src/main/java/algoblocks/view/buttons/CustomBlockButton.java
@@ -1,28 +1,23 @@
 package algoblocks.view.buttons;
 
-import algoblocks.view.workspace.WorkspaceView;
 import javafx.scene.control.Button;
 
 import algoblocks.engine.block.*;
 import algoblocks.controller.GameController;
+import algoblocks.controller.handlers.*;
+import algoblocks.view.workspace.WorkspaceView;
 
 import java.util.ArrayList;
 
 public class CustomBlockButton extends BlockButton {
     public CustomBlockButton(GameController game, WorkspaceView targetList, Block block) {
-        super(game, targetList, block);
-    }
-
-    @Override
-    public void action() {
-        targetList.addActionBlock(block);
-        game.addActionBlockToWorkspace(block);
+        super(new ActionBlockButtonHandler(game, targetList, block), block);
     }
 
     public static ArrayList<Button> createButtonArray(GameController game, WorkspaceView targetList, ArrayList<Block> blocks) {
         ArrayList<Button> buttons = new ArrayList<Button>();
         blocks.forEach((Block block) -> {
-            buttons.add(new ActionBlockButton(game, targetList, block));
+            buttons.add(new CustomBlockButton(game, targetList, block));
         });
         return buttons;
     }
